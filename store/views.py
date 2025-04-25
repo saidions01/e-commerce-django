b154e23d102c7a3353d024c3cc98983dfc08a158
saidions01 <101123304+saidions01@users.noreply.github.com>
2025-04-25 22:40:14 +0100

    second commit

--- store/views.py
+++ store/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Wishlist, Product
 from store import views
 import random
-@login_required
+
 def home(request):
     all_products = list(Product.objects.all())
     random_products = random.sample(all_products, min(len(all_products), 8))
@@ -18,16 +18,18 @@ def product_list(request):
 
 
 
-@login_required
+
 def wishlist_view(request):
-    wishlisted_items = Wishlist.objects.filter(user=request.user).select_related('product')
-    print("Wishlisted items:", wishlisted_items)
+    wishlisted_items = Wishlist.objects.select_related('product')  # no user filter
+   
     return render(request, 'wishlist.html', {'wishlisted_items': wishlisted_items})
-@login_required
+
+
 def add_to_wishlist(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     Wishlist.objects.get_or_create(user=request.user, product=product)
     return redirect('wishlist')
+
 
 @login_required
 def remove_from_wishlist(request, product_id):
